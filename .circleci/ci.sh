bcb0ae284777ab154c0f4aca04db45595ced6c2b
Jared Wasinger <j-wasinger@hotmail.com>
2020-06-19 18:14:44 +0000

    fix ci

--- .circleci/ci.sh
+++ .circleci/ci.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 set -e
 
-mkdir build
 solc --strict-assembly --optimize src/f6m_mul/test.yul | awk '/Binary representation:/ { getline; print $0 }' > build/test.bin
 evmc run --gas 100000000 --vm /root/libevmone.so $(cat build/test.bin) | python3 .circleci/catch_evmc_err.py
