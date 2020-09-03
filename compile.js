7678463a8d16d34c6302d260dc9e06b9fd201f06
poemm <36397285+poemm@users.noreply.github.com>
2020-09-03 15:51:21 -0500

    .

--- compile.js
+++ compile.js
@@ -6,12 +6,15 @@ const parser = require('./huff/src/parser');
 
 const pathToData = path.posix.resolve(__dirname, './');
 
-const { inputMap, macros, jumptables } = parser.parseFile('f2m_mul.huff', pathToData);
+//const { inputMap, macros, jumptables } = parser.parseFile('f2m_mul.huff', pathToData);
+const { inputMap, macros, jumptables } = parser.parseFile('f6m_mul.huff', pathToData);
 
 const {
     data: { bytecode: macroCode },
+//} = parser.processMacro('F2M_MUL_TESTER_HARDCODED', 0, [], macros, inputMap, jumptables);
 //} = parser.processMacro('F2M_MUL_TESTER_CALLDATA', 0, [], macros, inputMap, jumptables);
 } = parser.processMacro('F2M_MUL_BENCHER', 0, [], macros, inputMap, jumptables);
+//} = parser.processMacro('F6M_MUL_TESTER_HARDCODED', 0, [], macros, inputMap, jumptables);
 
 console.log("0x"+macroCode)
 
