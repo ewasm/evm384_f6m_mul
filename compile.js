e90eee765ea6f7a6d0022e63a53fe6fcae2331ab
poemm <36397285+poemm@users.noreply.github.com>
2020-09-11 23:27:16 -0500

    .

--- compile.js
+++ compile.js
@@ -8,14 +8,14 @@ const pathToData = path.posix.resolve(__dirname, './');
 
 // uncomment a one file and one macro name below
 
-//const { inputMap, macros, jumptables } = parser.parseFile('f2m_mul_v2.huff', pathToData);
+//const { inputMap, macros, jumptables } = parser.parseFile('f6m_mul_v2.huff', pathToData);
 //const { inputMap, macros, jumptables } = parser.parseFile('f6m_mul_v4.huff', pathToData);
-const { inputMap, macros, jumptables } = parser.parseFile('f6m_mul_v4.huff', pathToData);
+const { inputMap, macros, jumptables } = parser.parseFile('f6m_mul_v6.huff', pathToData);
 
 const {
     data: { bytecode: macroCode },
-//} = parser.processMacro('F6M_MUL_TEST_HARDCODED', 0, [], macros, inputMap, jumptables);
-} = parser.processMacro('F6M_MUL_BENCH', 0, [], macros, inputMap, jumptables);
+} = parser.processMacro('F6M_MUL_TEST_HARDCODED', 0, [], macros, inputMap, jumptables);
+//} = parser.processMacro('F6M_MUL_BENCH', 0, [], macros, inputMap, jumptables);
 
 console.log("0x"+macroCode)
 
