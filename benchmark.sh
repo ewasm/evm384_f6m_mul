a91a1a0c736405b91d12a6b3cc681ff04e40a274
Jared Wasinger <j-wasinger@hotmail.com>
2020-09-16 05:56:03 +0000

    add v7

--- benchmark.sh
+++ benchmark.sh
@@ -13,5 +13,7 @@ set -e
 ./deps/v4/evmone/build/bin/evmone-bench --benchmark_format=json --benchmark_color=false --benchmark_min_time=5 build/v4-huff-f6m_mul_bench.hex 00 ff | python3 catch_evmone_bench_err.py
 ./deps/v6/evmone/build/bin/evmone-bench --benchmark_format=json --benchmark_color=false --benchmark_min_time=5 build/v6-f6m_mul_bench.hex 00 ff | python3 catch_evmone_bench_err.py
 
+./deps/v7/evmone/build/bin/evmone-bench --benchmark_format=json --benchmark_color=false --benchmark_min_time=5 build/v7-f6m_mul_bench.hex "" "" | python3 catch_evmone_bench_err.py
+
 # TODO fix stock v2 huff implementation of f6m_mul
 # ./deps/v2/evmone/build/bin/evmone-bench --benchmark_format=json --benchmark_color=false --benchmark_min_time=5 build/v7-f6m_mul_bench.hex 00 00 | python3 catch_evmone_bench_err.py
